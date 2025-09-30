ca6b8e352506efd0254064633612959674eac598
weavermarquez <weavermarquez0@gmail.com>
2025-09-30 18:03:27 -0400

    install: step 7 set up /api/auth/* mount handler

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig } from "@react-router/dev/routes";
+import { index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("api/auth/*", "routes/api.auth.$.ts"),
+] satisfies RouteConfig;
---
+++ app/routes/api.auth.$.ts
@@ -0,0 +1,11 @@
+import { auth } from "../lib/auth"; // Adjust the path as necessary
+// import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
+import type { Route } from "./+types/api.auth.$.ts";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  return auth.handler(request);
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  return auth.handler(request);
+}
