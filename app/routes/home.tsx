0c0c677dc4e5a4a3e68f1533cfb401bfad31d48b
weavermarquez <weavermarquez0@gmail.com>
2025-10-03 10:23:04 -0400

    fix(client): restore sign-in functionality
    - neither dotenv/config nor process are meant to be run on the browser!
      they are server TS code only.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,36 +5,12 @@ import SignUp from "./signup";
 import SignIn from "./signin";
 
 import { ai, ax } from "@ax-llm/ax";
-import "dotenv/config";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
-}
-
-export async function loader({ params }: Route.LoaderArgs) {
-  const llm = ai({
-    name: "openai",
-    config: { model: "gpt-4o" },
-    apiKey: process.env.OPENAI_APIKEY!,
-  });
-
-  const basicPrompt = ax("conversation:string -> assistantResponse:string");
-
-  async function converse(inputMessage: string) {
-    const result = await basicPrompt.forward(llm, {
-      conversation: inputMessage,
-    });
-
-    console.log("Assistant:", result.assistantResponse);
-    return result.assistantResponse;
-  }
-
-  const modelResponse = await converse("Hello there, you're live on air!");
-
-  return { modelResponse };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
@@ -44,16 +20,14 @@ export default function Home({ loaderData }: Route.ComponentProps) {
     return (
       <div>
         <h1>Hello, {data.user.email}!</h1>
-        <h2>{loaderData.modelResponse}</h2>
       </div>
     );
   } else {
     return (
       <div>
         <SignIn />
-        <SignUp />
         <br />
-        <h2>AxLLM: {loaderData.modelResponse}</h2>
+        <SignUp />
       </div>
     );
   }
